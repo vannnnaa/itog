85a354c79a1d0cb90bde29b6c4179d7b6b7d3866
Valishina Anna <105487048+vannnnaa@users.noreply.github.com>
2024-11-05 19:48:54 +0300

    Add files via upload

--- dzpassport/change.js
+++ dzpassport/change.js
@@ -1,1 +1,21 @@
+const change = document.getElementById("change")
 
+function change_all(){
+
+    if (document.getElementsByTagName('div')[3].innerText==='Валишина'){
+        document.getElementsByTagName('div')[3].innerText='Valishina';
+        document.getElementsByTagName('div')[4].innerText='Anna';
+        document.getElementsByTagName('div')[7].innerHTML='123456';
+    return;}
+
+    if (document.getElementsByTagName('div')[3].innerText==='Valishina'){
+        document.getElementsByTagName('div')[3].innerText='Валишина';
+        document.getElementsByTagName('div')[4].innerText='Анна';
+        document.getElementsByTagName('div')[7].innerHTML='03.09.2005';
+        return;
+    }
+
+
+
+}
+change.addEventListener("click",change_all)
